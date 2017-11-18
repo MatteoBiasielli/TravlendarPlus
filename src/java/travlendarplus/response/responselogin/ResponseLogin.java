716316accc372dafd7c4cdb295281e5589e4bf1b
MatteoBiasielli <matteo.biasielli@mail.polimi.it>
2017-11-18 14:33:59 +0100

    Added response classes (login)

---
+++ src/java/travlendarplus/response/responselogin/ResponseLogin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travlendarplus.response.responselogin;
+
+import travlendarplus.response.Response;
+import travlendarplus.user.*;
+
+/**
+ *
+ * @author matteo
+ */
+public class ResponseLogin extends Response{
+    private ResponseLoginType rlt;
+    private User u;
+    ResponseLogin(ResponseLoginType r, User u){
+        this.rlt=r;
+        this.u=u;
+    }
+    public ResponseLoginType getType(){
+        return rlt;
+    }
+    public User getUser(){
+        return u;
+    }
+}
---
+++ src/java/travlendarplus/response/responselogin/ResponseLoginType.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travlendarplus.response.responselogin;
+
+/**
+ *
+ * @author matteo
+ */
+public enum ResponseLoginType {  
+    OK("ok"),
+    LOGIN_USERNAME_ERROR("The given username does not exist."),
+    LOGIN_PASSWORD_ERROR("Wrong password");
+    private String message;
+    
+    ResponseLoginType(String s){
+        this.message=s;
+    }
+    public String getMessage(){
+        return message;
+    }
+}

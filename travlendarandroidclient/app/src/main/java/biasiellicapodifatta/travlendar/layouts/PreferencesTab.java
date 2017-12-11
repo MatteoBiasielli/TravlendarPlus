cc8d4e4049e73a4a2cf6f3c790f51efaf6819002
emilioC33 <emilio.capo@mail.polimi.it>
2017-12-11 01:40:16 +0100

    Preferences tab layout done
    It must still be refined.

--- travlendarandroidclient/app/src/main/java/biasiellicapodifatta/travlendar/layouts/PreferencesTab.java
+++ travlendarandroidclient/app/src/main/java/biasiellicapodifatta/travlendar/layouts/PreferencesTab.java
@@ -21,6 +21,6 @@ public class PreferencesTab extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.map_tab_layout, container, false);
+        return inflater.inflate(R.layout.preferences_tab_layout, container, false);
     }
 }
